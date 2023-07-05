9e269d14cad089382e55682bf77741c1d660e4d3
tkcualong9881@gmail.com <tkcualong9881@gmail.com>
2023-07-05 08:18:51 +0700

    #

---
+++ UserControlLib/topdf.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Packaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Xps;
+using System.Windows.Xps.Packaging;
+
+namespace UserControlLib
+{
+    public class topdf
+    {
+        public static void ToPdf(FrameworkElement element, string filepath) 
+        {
+            /*
+             *  Convert WPF -> XPS -> PDF
+             */
+            MemoryStream lMemoryStream = new MemoryStream();
+            Package package = Package.Open(lMemoryStream, FileMode.Create);
+            XpsDocument doc = new XpsDocument(package);
+            XpsDocumentWriter writer = XpsDocument.CreateXpsDocumentWriter(doc);
+
+            // This is your window
+            writer.Write(element);
+
+            doc.Close();
+            package.Close();
+
+            // Convert 
+            MemoryStream outStream = new MemoryStream();
+            PdfSharp.Xps.XpsConverter.Convert(lMemoryStream, outStream, false);
+
+            // Write pdf file
+            FileStream fileStream = new FileStream(filepath, FileMode.Create);
+            outStream.CopyTo(fileStream);
+
+            // Clean up
+            outStream.Flush();
+            outStream.Close();
+            fileStream.Flush();
+            fileStream.Close();
+        }
+    }
+}
